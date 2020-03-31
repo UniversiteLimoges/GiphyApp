ef40c59c66cbdc11bbd373b5862e1dd14563670d
Quentin <solid@Solid-Peace.aircard>
2020-03-31 14:44:37 +0200

    add-tailwindcss

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,5 @@
+/*global require, Vue,*/
+/*eslint no-unused-vars: 1*/
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -24,7 +26,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 Vue.component('user-component', require('./components/UserComponent.vue').default);
 
 /**
- * Next, we will create a fresh Vue application instance and attach it to
+ * Next, we will create a fresh Vue application instance and attach it to	
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
